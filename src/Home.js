7d6987a23202563b14ac2cce4dc9c3f2532a26fc
RamyaKpradeep <ramyakumaresan2110@gmail.com>
2023-10-02 18:03:05 +0530

    fetch image added

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Home.css';
+function Home() {
+    return (
+        <div>
+            <div className='background'>
+                <div className='row'>
+                    <div className='col-12 col-sm-12 col-md-12 col-lg-12'>
+                        <h2 className='pt-5 text-white' style={{ fontWeight: '500',color:'blue' }}>Fetch Images from API</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
+
